4edb14ef2f11035326c93f46d987ee9b94957a83
gonagon4 <82447045+gonagon4@users.noreply.github.com>
2023-10-07 11:44:25 +0200

    test 2 falla, registrarse dejando un campo en blanco

--- Proyecto/test/test_bbdd.py
+++ Proyecto/test/test_bbdd.py
@@ -1,4 +1,6 @@
 import sys
+import pytest
+import mysql.connector
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.bbdd import DatabaseManager
@@ -7,5 +9,12 @@ from src.bbdd import DatabaseManager
 def test_register_user():
     db_manager = DatabaseManager('localhost', 'root', 'root', 'prueba')
     assert db_manager.register_user('test@example.com', 'password123') == True
+
+
+def test_register_user_exception():
+    db_manager = DatabaseManager("localhost", "root", "root", "prueba")
+    with pytest.raises(mysql.connector.Error):
+        db_manager.register_user("test@example.com", "")
+
 
 
