9bc22a494d9d0894c1fc6a6a78a84dfaacb1bf60
Juan Pablo Zurita <juanpablo.zuritasoto@usp.ceu.es>
2023-10-15 17:26:49 +0200

    Se abre automáticamente el navegador + cambios.

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import webbrowser
 from src import auth_bp
 
 app = Flask(__name__)
@@ -6,4 +7,8 @@ app.register_blueprint(auth_bp)
 app.testing = True
 
 if __name__ == '__main__':
+    url = "http://127.0.0.1:5000"
+
+    webbrowser.open(url)
+
     app.run(debug=True)
